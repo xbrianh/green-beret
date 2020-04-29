f56c6281c9f6bc45799dbd86247b032d72c783d5
Brian Hannafious <brian@connectedsignals.com>
2020-04-29 07:52:15 -0600

    better Make checks for correct env

--- common.mk
+++ common.mk
@@ -1,5 +1,18 @@
 SHELL=/bin/bash
 
 ifndef GREEN_BERET_HOME
-$(error Please run "source environment.[sfx]" in the green-beret repo root directory before running make commands)
+$(error Please run "source environment" in the green-beret repo root directory before running make commands)
+endif
+
+ifeq ($(shell which jq),)
+$(error Please install jq using "apt-get install jq" or "brew install jq")
+endif
+
+ifeq ($(findstring Python 3.8, $(shell python --version 2>&1)),)
+$(error Please run make commands from a Python 3.8 virtualenv)
+endif
+
+ifeq ($(findstring terraform, $(shell which terraform 2>&1)),)
+else ifeq ($(findstring Terraform v0.12.24, $(shell terraform --version 2>&1)),)
+$(error You must use Terraform v0.12.24, please check your terraform version.)
 endif
