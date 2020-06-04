cb605e05d0e9023ef608ada8440e1a6f9f1d62ae
Brian Hannafious <brian@connectedsignals.com>
2020-06-04 10:27:58 -0600

    Improve AWS temporary credentialing

--- scripts/generate_aws_sts_session.sh
+++ scripts/generate_aws_sts_session.sh
@@ -1,17 +1,8 @@
-#!/bin/bash
 # This is used to generate temporary credentials when using Terraform with MFA enabled
 # See issue: https://github.com/terraform-providers/terraform-provider-aws/issues/2420
-set -euo pipefail
-if [[ 1 != $# ]]; then
-    echo "Usage:"
-    echo "$0 role-arn"
-	exit 1
-fi
-role_arn=$1
+# Usage: `source generate_aws_sts_session.sh`
+role_arn="arn:aws:iam::422448306679:role/developer"
 role_json=$(aws sts assume-role --role-arn ${role_arn} --role-session-name bhannafi@ucsc.edu --duration-seconds 3600)
 export AWS_ACCESS_KEY_ID="$(echo ${role_json} | jq -r '.Credentials.AccessKeyId')"
 export AWS_SECRET_ACCESS_KEY="$(echo ${role_json} | jq -r '.Credentials.SecretAccessKey')"
 export AWS_SESSION_TOKEN="$(echo ${role_json} | jq -r '.Credentials.SessionToken')"
-echo "export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID"
-echo "export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY"
-echo "export AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN"
---
+++ scripts/revoke_aws_sts_session.sh
@@ -0,0 +1,5 @@
+# Revoke temporary credentials
+# usage: `source revoke_aws_sts_session.sh`
+unset AWS_ACCESS_KEY_ID
+unset AWS_SECRET_ACCESS_KEY
+unset AWS_SESSION_TOKEN
