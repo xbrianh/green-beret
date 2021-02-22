5207ad73b3de616801ee08a6f4358a4b92ae14fe
Brian Hannafious <xath@MacBook-Pro.local>
2021-02-22 11:13:23 -0700

    Update to Terraform 0.14.7

--- common.mk
+++ common.mk
@@ -8,11 +8,11 @@ ifeq ($(shell which jq),)
 $(error Please install jq using "apt-get install jq" or "brew install jq")
 endif
 
-ifeq ($(findstring Python 3.8, $(shell python --version 2>&1)),)
-$(error Please run make commands from a Python 3.8 virtualenv)
-endif
+# ifeq ($(findstring Python 3.8, $(shell python --version 2>&1)),)
+# $(error Please run make commands from a Python 3.8 virtualenv)
+# endif
 
 ifeq ($(findstring terraform, $(shell which terraform 2>&1)),)
-else ifeq ($(findstring Terraform v0.13.0, $(shell terraform --version 2>&1)),)
-$(error You must use Terraform v0.13.0, please check your terraform version.)
+else ifeq ($(findstring Terraform v0.14.7, $(shell terraform --version 2>&1)),)
+$(error You must use Terraform v0.14.7, please check your terraform version.)
 endif
